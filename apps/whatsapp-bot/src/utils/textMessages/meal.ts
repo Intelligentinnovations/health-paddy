52b1976d64cd7c30816a8ee772a161e58e9d58a0
abiodun <stepheng323@gmail.com>
2023-12-03 11:45:30 +0100

    spice-up-response-messages

--- apps/whatsapp-bot/src/utils/textMessages/meal.ts
+++ apps/whatsapp-bot/src/utils/textMessages/meal.ts
@@ -1,11 +1,12 @@
 import { GetCalorieGoalText } from "../../types"
 
-export const activityLevelText = `We are almost there, Please, Select your most appropriate activity level?\n
+export const getActivityLevelText = (goal: string) => `Congratulations on selecting the ${goal} option!🏋️🌱Let our meal plans support you in staying at your healthy weight, while still enjoying delightful and 
+flavorful dishes. Please, Select your most appropriate activity level?\n
 1. Sedentary (little to no regular exercise).\n
-2. Mild activity (intense exercise for at least 20 minutes 1-3 times per week. This may include things like brisk walking, bicycling, jogging, basketball, swimming etc. If you do not exercise regularly, but you maintain a busy life that requires you to walk frequently for long periods, you meet the requirements for this level).\n
+2. Mild activity (intense exercise for at least 20 minutes 1-3 times per week. This may include things like brisk walking 🚶‍♂️, bicycling 🚴‍♀️, jogging 🏃‍♂️, basketball🏀, swimming 🏊 etc. If you do not exercise regularly, but you maintain a busy life that requires you to walk frequently for long periods, you meet the requirements for this level).\n
 3. Moderate activity (intense exercise for 60 min 3 to 4 times per week. Any of the activities listed above will qualify)\n
-4. Heavy or labour-intensive activity (intense exercise for 60min or greater, 5 to 7 days per week. Labour intensive occupations also qualify for this level, such as bricklaying, carpentry, general labour, farming etc.).\n
-5. Extreme activity (Exceedingly active and/ or very demanding activities, such as athlete with an almost unstoppable training schedule, very demanding jobs such as shovelling coal or working long hours on an assembly line).\n`
+4. Heavy or labor-intensive activity (intense exercise for 60min or greater, 5 to 7 days per week. Labor intensive occupations also qualify for this level, such as bricklaying 🧱🔨, carpentry 🧱🔨, general labor 👷‍♂️💪, farming 🌾🚜etc.).\n
+5. Extreme activity (Exceedingly active and/ or very demanding activities, such as athlete with an almost unstoppable training schedule 🏋️‍♂️📅, very demanding jobs such as shovelling coal or working long hours on an assembly line)`
 
 
 export const extremeGainWeightText = `Trust me I get it, you want results really quick! But when you gain weight too quickly, you tend to gain a lot of unhealthy fat. Weight should be gained at a moderate pace of 1 - 2kg per month, you should either increase the duration or reduce your target weight\n
